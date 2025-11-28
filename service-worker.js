f44261a0963e0ba1869ed78ec2e2dbcfeb5658bd
Abbie <aitong0113@gmail.com>
2025-11-28 12:39:18 +0800

    pwa-joson

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+//即使 沒網路也能開啟網站
+
+const cacheName = "心天氣-v1";
+const filesToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./HeartWeather.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
